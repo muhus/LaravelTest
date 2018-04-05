0a7ca16629acd11b0949745a77eb062a889bb0cd
Mykhailo <muh@laravel-test.c.gentle-current-195415.internal>
2018-04-05 21:51:10 +0000

    styles & js added

--- resources/views/SBAdmin/partials/head.blade.php
+++ resources/views/SBAdmin/partials/head.blade.php
@@ -5,10 +5,10 @@
 <meta name="author" content="">
 <title>SB Admin - Start Bootstrap Template</title>
 <!-- Bootstrap core CSS-->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="SBAdminTheme/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- Custom fonts for this template-->
-<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="SBAdminTheme/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <!-- Page level plugin CSS-->
-<link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+<link href="SBAdminTheme/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 <!-- Custom styles for this template-->
-<link href="css/sb-admin.css" rel="stylesheet">
+<link href="SBAdminTheme/css/sb-admin.css" rel="stylesheet">
